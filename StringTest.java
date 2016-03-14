a42178df50f85354478ef481381ad0e1ec195562
RajeswariSabineni <Rajeswari.Sabineni@ge.com>
2016-03-14 13:48:39 -0400

    Added files via upload

---
+++ StringTest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.awt.*;
+
+
+
+public class StringTest extends Frame {
+public static void main(String a[]) {
+	System.out.println("Hello World...");
+	
+}
+}
+
+// i=5
+// j=6
